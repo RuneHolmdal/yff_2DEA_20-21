4c0d573f109de5960b5f70b38d628876e70889e9
Rune Holmdal <runehol@viken.no>
2020-11-10 08:13:53 +0100

    Grunnlag for undervisning - 10.11: Intro til array og indeks baserte variabler.

---
+++ arrOGlist/arrOGlist/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace arrOGlist
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(34, 35);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(471, 207);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(47, 290);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(47, 265);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(47, 265);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 50);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(47, 265);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(70, 50);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(255, 265);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(70, 50);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(145, 290);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(70, 50);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 395);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ arrOGlist/arrOGlist/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arrOGlist
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Deklarasjon av int tabell med de 7 verdiene: 2, 8, 9, 5, 7, 3 og 6  
+        int[] tabell1 = { 2, 8, 9, 5, 7, 3, 6 };
+        //indeks:         0  1  2  3  4  5  6 (posisjon/celle referanse)
+        //Length: 7 (antall verdier/celler/posisjoner)
+        //NB! Største indeks verdi er alltid Length - 1.
+
+        //Deklarasjon av float tabell med 10 verdier, som alle er 0
+        float[] tabell2 = new float[10];
+
+        //Deklarasjon av en Button tabell med plass til 6 Button's
+        Button[] b = new Button[6];
+
+        //Deklasrasjon av List med String
+        List<string> enStringListe = new List<string> {"desember", "jul", "advent", "kalender" };
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            EnkelTabellEks1();
+            TabellGjennomsnitt();
+            EnkelTabellEks2();
+            richTextBox1.Text +="Gjennomsnittsverdi: " + TabellGjennomsnittReturnert(tabell2);
+        }
+        private void EnkelTabellEks1()
+        {
+            //richTextBox1.Text = "" + tabell1[5];
+            //tabell1[0] = 10;
+            //richTextBox1.Text = "" + tabell1[5];
+            //tabell1[7] = 17;
+
+            for (int i = 0; i < tabell1.Length; i++)
+            {
+                richTextBox1.Text += tabell1[i] + ", ";
+            }
+            richTextBox1.Text += "\n"; // NB! Spesialtegnet \n = linjeskift
+        }
+
+        private void TabellGjennomsnitt()
+        {
+            int sum = 0;
+            float snitt = 0.0F;
+            for (int i = 0; i < tabell1.Length; i++)
+            {
+                sum += tabell1[i];
+            }
+            snitt = (float)sum / tabell1.Length;
+            richTextBox1.Text += "Sum: " + sum + ", Snitt: " + snitt + "\n";
+        }
+
+        private void EnkelTabellEks2()
+        {
+            //richTextBox1.Text += "\n" + tabell2[5];
+
+            // Fyller opp tabellen med tilfeldige desimaltall
+            Random r = new Random();
+            for (int i = 0; i < tabell2.Length; i++)
+            {
+                float nr = r.Next(100) * 2.53F;
+                if (i == 0)
+                    tabell2[i] = nr;
+                else
+                    tabell2[i] = i * nr;
+            }
+
+            // Skriver ut tallene i tabell2
+            for (int i = 0; i < tabell2.Length; i++)
+            {
+                richTextBox1.Text += tabell2[i] + "; ";
+            }
+            richTextBox1.Text += "\n";
+        }
+
+        private float TabellGjennomsnittReturnert(float[] t)
+        {
+            float sum = 0;
+            float snitt = 0;
+            for (int i = 0; i < t.Length; i++)
+            {
+                sum += t[i];
+            }
+            snitt = sum / t.Length;
+            return snitt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            b[0] = button1;
+            b[1] = button2;
+            b[2] = button3;
+            b[3] = button4;
+            b[4] = button5;
+            b[5] = button6;
+            for (int i = 0; i < b.Length-1; i++)
+            {
+                b[i + 1].Location = new Point(b[i].Location.X + 76, b[i].Location.Y);
+            }
+        }
+
+        bool vEnable = true;
+        private void button6_Click(object sender, EventArgs e)
+        {
+            vEnable = !vEnable;
+            foreach (Button bb in b)
+            {
+                if (bb.Name != "button6")
+                    bb.Visible = vEnable;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string[] t = { "hoho", "sjegg", "julepynt" };
+            enStringListe.Add("Julenisse");
+            foreach (string s in t)
+            {
+                enStringListe.Add(s);
+            }
+
+            richTextBox1.Text += "\n\n**************************\n";
+            foreach (string a in enStringListe)
+            {
+                richTextBox1.Text += a + "\n";
+            }
+        }
+    }
+}
