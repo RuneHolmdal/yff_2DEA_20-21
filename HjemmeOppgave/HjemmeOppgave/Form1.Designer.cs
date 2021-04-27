f6f2ed4c25059a75985348909d0176098b3fa515
Rune Holmdal <runehol@viken.no>
2021-04-27 08:00:13 +0200

    Fungerende løsningsforslag

---
+++ HjemmeOppgave/HjemmeOppgave/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿
+namespace HjemmeOppgave
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnRotKtrl = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnBeregn = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnFlytt = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRotKtrl
+            // 
+            this.btnRotKtrl.BackColor = System.Drawing.Color.Green;
+            this.btnRotKtrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRotKtrl.Location = new System.Drawing.Point(30, 129);
+            this.btnRotKtrl.Name = "btnRotKtrl";
+            this.btnRotKtrl.Size = new System.Drawing.Size(200, 91);
+            this.btnRotKtrl.TabIndex = 1;
+            this.btnRotKtrl.Text = "START";
+            this.btnRotKtrl.UseVisualStyleBackColor = false;
+            this.btnRotKtrl.Click += new System.EventHandler(this.btnRotKtrl_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HjemmeOppgave.Properties.Resources.wheels;
+            this.pictureBox1.Location = new System.Drawing.Point(327, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnBeregn
+            // 
+            this.btnBeregn.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnBeregn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBeregn.Location = new System.Drawing.Point(6, 18);
+            this.btnBeregn.Name = "btnBeregn";
+            this.btnBeregn.Size = new System.Drawing.Size(200, 91);
+            this.btnBeregn.TabIndex = 4;
+            this.btnBeregn.Text = "Beregn";
+            this.btnBeregn.UseVisualStyleBackColor = false;
+            this.btnBeregn.Click += new System.EventHandler(this.btnBeregn_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(20, 132);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(681, 219);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(734, 420);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnBeregn);
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(726, 387);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Oppg1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Controls.Add(this.btnRotKtrl);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(726, 387);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Oppg2";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.pictureBox2);
+            this.tabPage3.Controls.Add(this.label1);
+            this.tabPage3.Controls.Add(this.trackBar1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 29);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(726, 387);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Oppg3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::HjemmeOppgave.Properties.Resources.wheels;
+            this.pictureBox2.Location = new System.Drawing.Point(370, 35);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(121, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 37);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label2";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(55, 92);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(254, 69);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.pictureBox3);
+            this.tabPage4.Controls.Add(this.btnFlytt);
+            this.tabPage4.Location = new System.Drawing.Point(4, 29);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(726, 387);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Oppg4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::HjemmeOppgave.Properties.Resources.wheels;
+            this.pictureBox3.Location = new System.Drawing.Point(6, 6);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(162, 185);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btnFlytt
+            // 
+            this.btnFlytt.BackColor = System.Drawing.Color.Green;
+            this.btnFlytt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFlytt.Location = new System.Drawing.Point(577, 95);
+            this.btnFlytt.Name = "btnFlytt";
+            this.btnFlytt.Size = new System.Drawing.Size(98, 47);
+            this.btnFlytt.TabIndex = 8;
+            this.btnFlytt.Text = "-->";
+            this.btnFlytt.UseVisualStyleBackColor = false;
+            this.btnFlytt.Click += new System.EventHandler(this.btnFlytt_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 460);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.tabPage4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnRotKtrl;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnBeregn;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnFlytt;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ HjemmeOppgave/HjemmeOppgave/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HjemmeOppgave
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            xPos = pictureBox3.Location.X;
+            yPos = pictureBox3.Location.Y;
+        }
+        // Valuttaer                USD     EUR       GBP     BTC
+        double[] valuttaKurser = { 8.3637, 10.0423, 11.6432, 466783 };
+        double[] valuttaKjøp = { 4823, 26432, 77954 };
+        double[,] verdier = new double[3, 4];
+
+        // beregn hvor mye du får i hver valutta for de tre ulike vediene
+        private void btnBeregn_Click(object sender, EventArgs e)
+        {
+            verdier = res(valuttaKurser, valuttaKjøp);
+            /*
+            for (int i = 0; i < verdier.Length; i++)
+            {
+                for (int j = 0; j < valuttaKurser.Length; j++)
+                {
+                    resultat[i, j] = verdier[i] * valuttaKurser[j];
+                }
+            }
+            */
+            Utskrift();
+        }
+
+        private void Utskrift()
+        {
+            richTextBox1.Text = "NOK\t\tUSD\t\tEUR\t\tGBP\t\tBTC\n";
+            for (int x = 0; x < 3; x++)
+            {
+                richTextBox1.Text += valuttaKjøp[x];
+                for (int y = 0; y < 4; y++)
+                {
+                    richTextBox1.Text += "\t" + verdier[x, y];
+                }
+                richTextBox1.Text += "\n";
+            }
+        }
+
+        double[,] res(double[] kurs, double[] sum)
+        {
+            double[,] r = new double[3, 4];
+            for (int i = 0; i < sum.Length; i++)
+            {
+                for (int j = 0; j < kurs.Length; j++)
+                {
+                    r[i, j] = Math.Round(sum[i] / kurs[j], 2);
+                }
+            }
+            return r;
+        }
+
+        private void btnRotKtrl_Click(object sender, EventArgs e)
+        {
+            if (!timer1.Enabled)
+            {
+                btnRotKtrl.BackColor = Color.Red;
+                btnRotKtrl.Text = "STOP";
+                timer1.Start(); // endrer timer1.Enabled til true
+            }
+            else
+            {
+                btnRotKtrl.BackColor = Color.Green;
+                btnRotKtrl.Text = "START";
+                timer1.Stop(); // endrer timer1.Enabled til false
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            RoterPB1();
+        }
+
+        private void RoterPB1()
+        {
+            Image b = pictureBox1.Image;
+            b.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            pictureBox1.Image = b;
+        }
+        private void RoterPB2()
+        {
+            Image b = pictureBox2.Image;
+            b.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            pictureBox2.Image = b;
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            timer2.Start();
+
+            if (trackBar1.Value == 0)
+            {
+                timer2.Stop();
+            }
+            if (trackBar1.Maximum - trackBar1.Value > 0)
+            { 
+                timer2.Interval = (trackBar1.Maximum - trackBar1.Value) * 50;
+            }
+            else
+            {
+                timer2.Interval = 40;
+            }
+            label1.Text = Convert.ToString(trackBar1.Value) + ", " + trackBar1.Value * 17.4 + " km/h";
+        }
+
+        private void btnFlytt_Click(object sender, EventArgs e)
+        {
+            timer3.Start();
+        }
+
+        // xPos og yPos benyttes bare her. Setter PB2 i utgangsposisjon
+        // Utgangsposisjons verdiene tilordnes i public Form1()
+        int xPos, yPos;
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            RoterPB2();
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            Flytt();
+        }
+
+        private void Flytt()
+        {
+            if (!pictureBox3.Bounds.Contains(btnFlytt.Bounds))
+                pictureBox3.Location = new Point(pictureBox3.Location.X + 5, pictureBox3.Location.Y);
+            else
+            {
+                pictureBox3.Location = new Point(xPos, yPos);
+                timer3.Stop();
+            }
+        }
+    }
+}
